a36e0372db5106405c08743b12c480ced12ffd9e
Alexis M Vasquez <vasquez.a712@gmail.com>
2026-06-05 13:30:17 -0400

    Delete hello.cbl

--- programs/hello.cbl
+++
@@ -1,7 +0,0 @@
-       IDENTIFICATION DIVISION.
-       PROGRAM-ID. HELLO-WORLD.
-
-       PROCEDURE DIVISION.
-           DISPLAY "Hello, world!".
-           DISPLAY "My name is COBOL.".
-           STOP RUN.
